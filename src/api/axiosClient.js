fa9b6335829baba9b536b6bdccf682bb21272d8d
Nguyễn Văn Tĩnh <nguyenvantinh06@gmail.com>
2022-01-21 14:04:06 +0700

    + Update Ip localhost

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,7 +4,7 @@ import {Platform} from 'react-native';
 
 const axiosClient = axios.create({
   // baseURL: Platform.OS === 'android' ? process.env.REACT_APP_API_URL_ANDROID : process.env.REACT_APP_API_IOS,
-  baseURL: Platform.OS === 'android' ? 'http://192.168.1.16:5000/api' : 'https://192.168.1.16:5000/api',
+  baseURL: Platform.OS === 'android' ? 'http://192.168.1.8:5000/api' : 'https://192.168.1.8:5000/api',
   headers: {
     "content-type": "application/json",
   },
--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -1,14 +1,40 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+// import React from "react";
+// import { StyleSheet, Text, View } from "react-native";
 
-const Account = () => {
-  return (
-    <View>
-      <Text>This is account</Text>
-    </View>
-  );
-};
+// const Account = () => {
+//   return (
+//     <View>
+//       <Text>This is account</Text>
+//     </View>
+//   );
+// };
+
+// export default Account;
+
+// const styles = StyleSheet.create({});
+
+import React from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+
+const Account = () => (
+  <View style={[styles.container, styles.horizontal]}>
+    <ActivityIndicator />
+    <ActivityIndicator size="large" />
+    <ActivityIndicator size="large" color="#0000ff" />
+    <ActivityIndicator size="large" color="#00ff00" />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
+  }
+});
 
 export default Account;
-
-const styles = StyleSheet.create({});
