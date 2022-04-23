d122f2cac45fd1a9302e56dc83d0eac0688fce93
Nguyễn Văn Tĩnh <nguyenvantinh06@gmail.com>
2022-04-23 15:12:50 +0700

    Transfor code to android native

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
 import SignStackNavigation from "./src/navigations/SignStackNavigation";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import 'expo-dev-client'
 
 import store from "./src/redux/store";
 
